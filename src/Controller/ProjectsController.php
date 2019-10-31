55480948379015352ba3cb452e42d1f79db40e29
AugustinDmrt <damourette.augustin@gmail.com>
2019-10-31 14:54:24 +0100

    Project finis

---
+++ src/Controller/ProjectsController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProjectsController extends AbstractController
+{
+    /**
+     * @Route("/index", name="projects")
+     */
+    public function index()
+    {
+        $projects =array();
+        $projects[0] = 'project 1';
+        $projects[1] = 'project 2';
+        $projects[2] = 'project 3';
+        return $this->render('projects/index.html.twig', [
+            'controller_name' => 'ProjectsController',
+            'projects' => $projects,
+        ]);
+    }
+}
